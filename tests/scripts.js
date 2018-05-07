6f8fadc081ac3649ffb2b72f4277552df76ce851
crashdemons <crashenator+gh@gmail.com>
2018-05-07 03:21:53 -0500

    Added some tests that can be run. (may need discussion on how to approach some of these)

---
+++ tests/scripts.js
@@ -0,0 +1,38 @@
+window.testPgpLock=new Promise(function(r,f){r('dummy');});//create a dummy promise so we can chain successive tests onto it sequentially.
+
+
+function addResult_callback(testcase){
+	console.log('add result');
+	var result_string = '<span style="color:red;font-weight:bold;">FAIL</span>';
+	if(testcase.result) result_string = '<span style="color:green;font-weight:bold;">PASS</span>';
+	$("#results").append("<tr><td>"+testcase.id+"</td><td>"+testcase.name+"</td><td>"+testcase.expectation+"</td><td>"+testcase.actual+"</td><td>"+result_string+"</td></tr>");
+}
+
+
+function testPgpInput(input_str,resolve,reject){
+	$("#input_text").val(input_str);
+	window.input_check=true;
+	window.input_dirty=true;
+	var retval = input_verify();
+	resolve(retval);
+	return retval;
+}
+
+function testPgpInput_delayed(input_str){
+
+	window.testPgpLock = window.testPgpLock.then(function(){
+		return new Promise(function(resolve, reject) {
+			setTimeout(testPgpInput, 260, input_str, resolve,reject);
+		});
+	});
+	return window.testPgpLock;
+}
+
+$(function(){
+	tester.load_cases('pgp_cases.json',function(){
+		$('#status').text('Running test-cases...');
+		tester.test_cases(testPgpInput_delayed, addResult_callback,function(){
+			$('#status').text('Tests Ran.');
+		});
+	});
+});
---
+++ tests/tester.js
@@ -0,0 +1,68 @@
+function isPromise(value) {
+  return value && Object.prototype.toString.call(value) === "[object Promise]";
+}
+
+var tester = {
+	cases : [],
+	cases_ready: 0,
+	load_cases : function(url, loaded_callback=null){
+		this.cases_ready=0;
+		var tester_instance = this;
+		$.getJSON(url,null,function(data){
+			tester_instance.cases = data;
+			//loaded_callback();
+			tester_instance.cases.forEach(function(element,i){
+				tester_instance.load_case_data(i,function(){
+					console.log('loaded! '+(tester_instance.cases_ready))
+					tester_instance.cases_ready+=1;
+
+					if(tester_instance.cases_ready == tester_instance.cases.length && loaded_callback!==null)
+						loaded_callback();
+
+
+				});
+			});
+		})
+	},
+	load_case_data : function(i, loaded_callback){
+		var filename = "./cases/" + this.cases[i].id.toString() + ".txt"
+		var tester_instance = this;
+		$.ajax({
+		  dataType: "text",
+		  url: filename,
+		  data: null,
+		  success: function(data){
+			//console.log(data)
+			//console.log(tester_instance.cases[i]);
+			tester_instance.cases[i].input=data;
+			loaded_callback();
+		  }
+		});
+	},
+	test_cases : function(test_function,test_callback=null,test_finished=null){
+		var tester_instance = this;
+		this.cases.forEach(function(element,i){
+			tester_instance.test_case(i, test_function, test_callback);
+		});
+		if(test_finished!==null) test_finished();//foreach is synchronous so this will be when the tests are 'ran', but a test function itself may be async
+	},
+	test_case : function(i, test_function, test_callback=null){
+		var tester_instance = this;
+		var test_output = test_function(this.cases[i].input);
+
+		if(isPromise(test_output)){//if we get a promise (async function still running), wait for the result to complete the test-case
+			console.log(test_output);
+			test_output.then(function(final_output){
+				tester_instance.test_case(i,function(){
+					console.log(final_output);
+					return final_output;
+				},test_callback);
+			});
+			return;
+		}
+
+		this.cases[i].actual = test_output;
+		this.cases[i].result = (this.cases[i].expectation === this.cases[i].actual);
+		if(test_callback!==null) test_callback(this.cases[i])
+	}
+}
